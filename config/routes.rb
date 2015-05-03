c84b5f6a79c6bf8b2601587b906e2d93622f16f9
nimrodeln <nimrodeln@gmail.com>
2015-05-03 23:23:50 +0200

    esta version incluye el funcionamiento completo de una lista de TODOs

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  Rails.application.routes.draw do
+ 
+    resources :todos
+ 
+    root 'welcome#index'
+  end
+  
 end
